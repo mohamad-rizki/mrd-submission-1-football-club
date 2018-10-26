368b6a872d18056a8c9c4b24d89c62cd3f1f8df1
Mohamad Rizki Dafianto <mohamad.rizki@fusi24.com>
2018-10-26 17:23:45 +0700

    Add Detail UI

--- app/src/main/java/com/dafian/android/submission1footballclub/data/FootballClub.kt
+++ app/src/main/java/com/dafian/android/submission1footballclub/data/FootballClub.kt
@@ -1,3 +1,33 @@
 package com.dafian.android.submission1footballclub.data
 
-data class FootballClub(val name: String?, val description: String?, val image: Int?)
+import android.os.Parcel
+import android.os.Parcelable
+
+data class FootballClub(val name: String?, val description: String?, val image: Int?) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readValue(Int::class.java.classLoader) as? Int
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(description)
+        parcel.writeValue(image)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<FootballClub> {
+        override fun createFromParcel(parcel: Parcel): FootballClub {
+            return FootballClub(parcel)
+        }
+
+        override fun newArray(size: Int): Array<FootballClub?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/java/com/dafian/android/submission1footballclub/ui/detail/FootballDetailActivity.kt
+++ app/src/main/java/com/dafian/android/submission1footballclub/ui/detail/FootballDetailActivity.kt
@@ -1,18 +1,38 @@
 package com.dafian.android.submission1footballclub.ui.detail
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import org.jetbrains.anko.AnkoComponent
-import org.jetbrains.anko.AnkoContext
+import android.view.Gravity
+import android.widget.ImageView
+import android.widget.TextView
+import com.dafian.android.submission1footballclub.GlideApp
+import com.dafian.android.submission1footballclub.data.FootballClub
+import org.jetbrains.anko.imageView
+import org.jetbrains.anko.matchParent
+import org.jetbrains.anko.textView
 import org.jetbrains.anko.verticalLayout
 
 class FootballDetailActivity : AppCompatActivity() {
 
-    class FootballDetailUI : AnkoComponent<FootballDetailActivity> {
+    private lateinit var ivLogo: ImageView
+    private lateinit var tvName: TextView
+    private lateinit var tvNote: TextView
 
-        override fun createView(ui: AnkoContext<FootballDetailActivity>) = with(ui) {
-            verticalLayout {
-
-            }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        verticalLayout {
+            lparams(width = matchParent, height = matchParent)
+            gravity = Gravity.CENTER_HORIZONTAL
+            ivLogo = imageView()
+            tvName = textView()
+            tvNote = textView()
         }
+
+        val intent = intent
+        val item = intent.getParcelableExtra<FootballClub>("club")
+
+        tvName.text = item.name
+        tvNote.text = item.description
+        item.image?.let { GlideApp.with(this).load(it).into(ivLogo) }
     }
 }
